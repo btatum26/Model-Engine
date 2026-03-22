0c784c7cfe1c09b5c89603ad9256584cd9a73634
btatum26 <btatum26@vt.edu>
2026-03-22 10:15:49 +0100

    added hyper parameter support as well

--- src/strategies/momentum_surge/model.py
+++ src/strategies/momentum_surge/model.py
@@ -6,12 +6,13 @@ from src.controller import SignalModel
 
 class MomentumSurge(SignalModel):
     def generate_signals(self, df, params):
-        # Use auto-generated context
+        # Use auto-generated context for robustness and IDE support
         # RSI_14 will map to "RSI_14" in df
         rsi_val = df[ctx.RSI_14]
         
-        condition_long = (rsi_val < params['rsi_lower'])
-        condition_short = (rsi_val > params['rsi_upper'])
+        # Use ctx for hyperparameter keys as well
+        condition_long = (rsi_val < params[ctx.RSI_LOWER])
+        condition_short = (rsi_val > params[ctx.RSI_UPPER])
         
         signals = np.select(
             [condition_long, condition_short], 
